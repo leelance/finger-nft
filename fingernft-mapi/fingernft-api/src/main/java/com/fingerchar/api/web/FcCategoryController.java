9bb4c28a81b543ce2cd39fbe3124d73d65629e36
lance <li6151770@126.com>
2024-09-16 19:29:58 +0800

    update category/home

--- fingernft-mapi/fingernft-api/src/main/java/com/fingerchar/api/web/FcCategoryController.java
+++ fingernft-mapi/fingernft-api/src/main/java/com/fingerchar/api/web/FcCategoryController.java
@@ -1,31 +1,33 @@
 package com.fingerchar.api.web;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.fingerchar.api.service.FcNftCategoryService;
 import com.fingerchar.core.base.controller.BaseController;
 import com.fingerchar.core.constant.SysConfConstant;
 import com.fingerchar.core.util.ResponseUtil;
 import com.fingerchar.db.domain.FcNftCategory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+/**
+ * @author admin
+ * @since 2024/9/16 19:03
+ */
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(SysConfConstant.URL_PREFIX + "/category")
 public class FcCategoryController extends BaseController {
+  private final FcNftCategoryService categoryService;
 
-    @Autowired
-    private FcNftCategoryService categoryService;
-
-    /*
-    * 展示全部分类
-    * */
-    @PostMapping("/list")
-    public Object listAllSelectiveCategory() {
-        List<FcNftCategory> categories = categoryService.findAll();
-        return ResponseUtil.ok(categories);
-    }
+  /*
+   * 展示全部分类
+   * */
+  @PostMapping("/list")
+  public Object listAllSelectiveCategory() {
+    List<FcNftCategory> categories = categoryService.findAll();
+    return ResponseUtil.ok(categories);
+  }
 }
--- fingernft-mapi/fingernft-api/src/main/java/com/fingerchar/api/web/FcHomeController.java
+++ fingernft-mapi/fingernft-api/src/main/java/com/fingerchar/api/web/FcHomeController.java
@@ -1,68 +1,72 @@
 package com.fingerchar.api.web;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.fingerchar.api.service.*;
+import com.fingerchar.api.service.FcContractNftService;
+import com.fingerchar.api.service.FcUserService;
 import com.fingerchar.api.vo.HomeIndexParamsVO;
 import com.fingerchar.core.base.controller.BaseController;
 import com.fingerchar.core.constant.SysConfConstant;
 import com.fingerchar.core.util.ResponseUtil;
 import com.fingerchar.db.domain.FcUser;
 import com.fingerchar.db.vo.NftInfoVo;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * HomeController
+ *
+ * @author admin
+ * @since 2024/9/16 19:04
+ */
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(SysConfConstant.URL_PREFIX + "/home")
 public class FcHomeController extends BaseController {
+  private final FcContractNftService contractNftService;
+  private final FcUserService userService;
 
-	@Autowired
-	FcContractNftService contractNftService;
+  /**
+   * 获取售卖中的nft
+   */
+  @PostMapping("list")
+  public Object list(HomeIndexParamsVO params) {
+    IPage<NftInfoVo> iPage = contractNftService.findListForIndex(this.getPageInfo(), params);
+    return ResponseUtil.okList(iPage);
+  }
 
-	@Autowired
-	FcUserService userService;
+  /**
+   * 通过name搜索nft列表
+   *
+   * @param search fc_contract_nft表中的name字段
+   */
+  @PostMapping("search")
+  public Object search(String search) {
 
-	/**
-	 *获取售卖中的nft
-	 * @param params
-	 * @return
-	 */
-	@PostMapping("list")
-	public Object list(HomeIndexParamsVO params) {
-		IPage<NftInfoVo> iPage = contractNftService.findListForIndex(this.getPageInfo(), params);
-		return ResponseUtil.okList(iPage);
-	}
+    if (StringUtils.isEmpty(search)) {
+      search = "";
+    }
+    return contractNftService.findSearch(search, this.getPageInfo());
+  }
 
-	/**
-	 * 通过name搜索nft列表
-	 * @param search fc_contract_nft表中的name字段
-	 * @return
-	 */
-	@PostMapping("search")
-	public Object search(String search) {
+  /**
+   * 根据用户名模糊查找用户
+   *
+   * @param search 用户名
+   */
+  @PostMapping("searchuser")
+  public Object searchUser(String search) {
+    if (search == null) {
+      return ResponseUtil.okList(this.getPageInfo());
+    }
 
-		if(StringUtils.isEmpty(search)){
-			search = "";
-		}
-		return contractNftService.findSearch(search, this.getPageInfo());
-	}
-
-	/**
-	 * 根据用户名模糊查找用户
-	 * @param search 用户名
-	 * @return
-	 */
-	@PostMapping("searchuser")
-	public Object searchUser(String search) {
-		if(search == null){
-			return ResponseUtil.okList(this.getPageInfo());
-		}
-
-		IPage<FcUser> list = this.userService.findByName(search, this.getPageInfo());
-		return ResponseUtil.okList(list);
-	}
+    IPage<FcUser> list = this.userService.findByName(search, this.getPageInfo());
+    return ResponseUtil.okList(list);
+  }
 
 
 }
