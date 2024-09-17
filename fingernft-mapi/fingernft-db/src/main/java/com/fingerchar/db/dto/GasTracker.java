19aecf434fb35dcca6babb5ebe7d8737218d5523
lance <li6151770@126.com>
2024-09-17 10:02:40 +0800

    lombok

--- fingernft-mapi/fingernft-db/src/main/java/com/fingerchar/db/dto/GasTracker.java
+++ fingernft-mapi/fingernft-db/src/main/java/com/fingerchar/db/dto/GasTracker.java
@@ -1,54 +1,28 @@
 package com.fingerchar.db.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigInteger;
 
 /**
- * @Author： Zjm
- * @Date：2022/4/12 19:27
+ * GasTracker
+ *
+ * @author Zjm
+ * @since 2024/9/17 10:01
  */
+@Data
+@NoArgsConstructor
 public class GasTracker {
-    private String low;
-    private String medium;
-    private String high;
+  private String low;
+  private String medium;
+  private String high;
+  private String lastBlock;
 
-    public String getLastBlock() {
-        return lastBlock;
-    }
-
-    public void setLastBlock(String lastBlock) {
-        this.lastBlock = lastBlock;
-    }
-
-    private String lastBlock;
-
-    public GasTracker(BigInteger low, BigInteger medium, BigInteger high, BigInteger lastBlock){
-        this.low = low.toString();
-        this.medium = medium.toString();
-        this.high = high.toString();
-        this.lastBlock = lastBlock.toString();
-    }
-
-    public String getLow() {
-        return low;
-    }
-
-    public void setLow(String low) {
-        this.low = low;
-    }
-
-    public String getMedium() {
-        return medium;
-    }
-
-    public void setMedium(String medium) {
-        this.medium = medium;
-    }
-
-    public String getHigh() {
-        return high;
-    }
-
-    public void setHigh(String high) {
-        this.high = high;
-    }
+  public GasTracker(BigInteger low, BigInteger medium, BigInteger high, BigInteger lastBlock) {
+    this.low = low.toString();
+    this.medium = medium.toString();
+    this.high = high.toString();
+    this.lastBlock = lastBlock.toString();
+  }
 }
