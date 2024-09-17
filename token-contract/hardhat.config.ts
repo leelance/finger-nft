1c44339b6859ab5030568692f9c3fd1781a0d5c7
lance <li6151770@126.com>
2024-09-17 11:38:36 +0800

    update

--- token-contract/hardhat.config.ts
+++ token-contract/hardhat.config.ts
@@ -98,7 +98,7 @@ const config: HardhatUserConfig = {
       url: "http://192.168.204.140:8545",
       chainId: chainIds.besu,
       //accounts: ['0x811bB0C5EB3664D7bB167Ebc1cD77322586B7f89','0x77a4893b35753CBaA85915B538B1511E206316A7','0x6bd31D0177fa394Ac4c04FbfF7Dd140c38b44635'],
-      accounts: ['0x2d610c01187ae1ca8d93aeef276ade44c5e1e2df307e09d815f1db0ed0065fd6', '0x9ae40786418e1b68a5e5ce8917e564a678ac8084a1a010f89baeb8780bc21295', '0x643dcdabb9916b551baaebe7e21af860e54fc055eaef4f0d58bb40d8731ad806']
+      accounts: ['0x2d610c01188ae1ca8d93aeef276ade44c5e1e2df308e09d815f1db0ed0065fd6', '0x9ae40786418e1b68a5e5ce8917e564a678ac8084a1a010f89baeb8780bc21295', '0x643dcdabb9916b551baaebe7e21af860e54fc055eaef4f0d58bb40d8731ad806']
     },
   },
   paths: {
