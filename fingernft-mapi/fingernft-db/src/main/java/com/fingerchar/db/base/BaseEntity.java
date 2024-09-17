a48453aeb13970606be0f1ad8de2c55cf1d40d94
lance <li6151770@126.com>
2024-09-17 08:06:50 +0800

    lombok

--- fingernft-mapi/fingernft-db/src/main/java/com/fingerchar/db/base/BaseEntity.java
+++ fingernft-mapi/fingernft-db/src/main/java/com/fingerchar/db/base/BaseEntity.java
@@ -4,59 +4,35 @@ import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
 
+/**
+ * BaseEntity
+ *
+ * @author admin
+ * @since 2024/9/17 8:06
+ */
+@Data
 public class BaseEntity {
 
-	@TableId(type = IdType.AUTO)
-    protected Long id;
+  @TableId(type = IdType.AUTO)
+  protected Long id;
 
-	@TableField(value="deleted")
-    protected Boolean deleted;
+  @TableField(value = "deleted")
+  protected Boolean deleted;
 
-    @TableField(value="`create_time`", fill = FieldFill.INSERT)
-    protected Long createTime;
+  @TableField(value = "`create_time`", fill = FieldFill.INSERT)
+  protected Long createTime;
 
-    @TableField(value="`update_time`", fill = FieldFill.INSERT_UPDATE)
-    protected Long updateTime;
+  @TableField(value = "`update_time`", fill = FieldFill.INSERT_UPDATE)
+  protected Long updateTime;
 
-    public Long getId() {
-        return id;
-    }
+  public static final String ID = "id";
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public static final String DELETED = "deleted";
 
-    public Boolean getDeleted() {
-        return deleted;
-    }
+  public static final String CREATE_TIME = "create_time";
 
-    public void setDeleted(Boolean deleted) {
-        this.deleted = deleted;
-    }
-
-    public Long getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Long createTime) {
-        this.createTime = createTime;
-    }
-
-    public Long getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Long updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public static final String ID = "id";
-
-    public static final String DELETED = "deleted";
-
-    public static final String CREATE_TIME = "create_time";
-
-    public static final String UPDATE_TIME = "update_time";
+  public static final String UPDATE_TIME = "update_time";
 
 }
--- fingernft-mapi/fingernft-db/src/main/java/com/fingerchar/db/domain/FcContract.java
+++ fingernft-mapi/fingernft-db/src/main/java/com/fingerchar/db/domain/FcContract.java
@@ -1,312 +1,157 @@
 package com.fingerchar.db.domain;
 
-import com.fingerchar.db.base.BaseEntity;
-import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.fingerchar.db.base.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
- * @Description FcContract
- * @Author 
- * @Date 2022-06-10
- * @Version 2.1
+ * FcContract
+ *
+ * @author admin
+ * @since 2024/9/17 8:03
  */
+@Data
+@EqualsAndHashCode(callSuper = true)
 @TableName("fc_contract")
 public class FcContract extends BaseEntity {
+  
+  /**
+   * nft name
+   */
+  @TableField("`name`")
+  private String name;
+
+  /**
+   * nft symbol
+   */
+  @TableField("`symbol`")
+  private String symbol;
+
+  /**
+   * 合约地址
+   */
+  @TableField("`address`")
+  private String address;
+
+  /**
+   * 短地址
+   */
+  @TableField("`short_url`")
+  private String shortUrl;
+
+  /**
+   * 合约版本
+   */
+  @TableField("`version`")
+  private String version;
+
+  /**
+   * 图标
+   */
+  @TableField("`cover`")
+  private String cover;
 
+  /**
+   * 图标保存Id
+   */
+  @TableField("`storage_id`")
+  private Long storageId;
 
-    /**
-     * nft name
-     */
-    @TableField("`name`")
-    private String name;
-
-    /**
-     * nft symbol
-     */
-    @TableField("`symbol`")
-    private String symbol;
-
-    /**
-     * 合约地址
-     */
-    @TableField("`address`")
-    private String address;
-
-    /**
-     * 短地址
-     */
-    @TableField("`short_url`")
-    private String shortUrl;
-
-    /**
-     * 合约版本
-     */
-    @TableField("`version`")
-    private String version;
-
-    /**
-     * 图标
-     */
-    @TableField("`cover`")
-    private String cover;
-
-    /**
-     * 图标保存Id
-     */
-    @TableField("`storage_id`")
-    private Long storageId;
-
-    /**
-     * 合约拥有者
-     */
-    @TableField("`owner`")
-    private String owner;
-
-    /**
-     * 是否是官方合约
-     */
-    @TableField("`is_admin`")
-    private Boolean isAdmin;
-
-    /**
-     * 是否已验证
-     */
-    @TableField("`verify`")
-    private Boolean verify;
-
-    /**
-     * 合约描述
-     */
-    @TableField("`description`")
-    private String description;
-
-    /**
-     * 上一次增发的tokenId
-     */
-    @TableField("`last_token_id`")
-    private Long lastTokenId;
-
-    /**
-     * 封面
-     */
-    @TableField("`banner_url`")
-    private String bannerUrl;
-
-    /**
-     * 获取name和symbol次数
-     */
-    @TableField("`get_info_times`")
-    private Integer getInfoTimes;
-
-    /**
-     * 是否支持版权
-     */
-    @TableField("`is_royalties`")
-    private Boolean isRoyalties;
-
-    /**
-     * 签名人账号
-     */
-    @TableField("`signer`")
-    private String signer;
-
-    /**
-     * 是否已经同步
-     */
-    @TableField("`is_sync`")
-    private Boolean isSync;
+  /**
+   * 合约拥有者地址
+   */
+  @TableField("`owner`")
+  private String owner;
 
+  /**
+   * 是否是官方合约
+   */
+  @TableField("`is_admin`")
+  private Boolean isAdmin;
 
-    public String getName() {
-        return name;
-    }
+  /**
+   * 是否已验证
+   */
+  @TableField("`verify`")
+  private Boolean verify;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  /**
+   * 合约描述
+   */
+  @TableField("`description`")
+  private String description;
 
-    public String getSymbol() {
-        return symbol;
-    }
+  /**
+   * 上一次增发的tokenId
+   */
+  @TableField("`last_token_id`")
+  private Long lastTokenId;
 
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
-    }
+  /**
+   * 封面
+   */
+  @TableField("`banner_url`")
+  private String bannerUrl;
 
-    public String getAddress() {
-        return address;
-    }
+  /**
+   * 获取name和symbol次数
+   */
+  @TableField("`get_info_times`")
+  private Integer getInfoTimes;
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+  /**
+   * 是否支持版权
+   */
+  @TableField("`is_royalties`")
+  private Boolean isRoyalties;
 
-    public String getShortUrl() {
-        return shortUrl;
-    }
+  /**
+   * 签名人账号
+   */
+  @TableField("`signer`")
+  private String signer;
 
-    public void setShortUrl(String shortUrl) {
-        this.shortUrl = shortUrl;
-    }
+  /**
+   * 是否已经同步
+   */
+  @TableField("`is_sync`")
+  private Boolean isSync;
 
-    public String getVersion() {
-        return version;
-    }
 
-    public void setVersion(String version) {
-        this.version = version;
-    }
+  public static final String NAME = "`name`";
 
-    public String getCover() {
-        return cover;
-    }
+  public static final String SYMBOL = "`symbol`";
 
-    public void setCover(String cover) {
-        this.cover = cover;
-    }
+  public static final String ADDRESS = "`address`";
 
-    public Long getStorageId() {
-        return storageId;
-    }
+  public static final String SHORT_URL = "`short_url`";
 
-    public void setStorageId(Long storageId) {
-        this.storageId = storageId;
-    }
+  public static final String VERSION = "`version`";
 
-    public String getOwner() {
-        return owner;
-    }
+  public static final String COVER = "`cover`";
 
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
+  public static final String STORAGE_ID = "`storage_id`";
 
-    public Boolean getIsAdmin() {
-        return isAdmin;
-    }
+  public static final String OWNER = "`owner`";
 
-    public void setIsAdmin(Boolean isAdmin) {
-        this.isAdmin = isAdmin;
-    }
+  public static final String IS_ADMIN = "`is_admin`";
 
-    public Boolean getVerify() {
-        return verify;
-    }
+  public static final String VERIFY = "`verify`";
 
-    public void setVerify(Boolean verify) {
-        this.verify = verify;
-    }
+  public static final String DESCRIPTION = "`description`";
 
-    public String getDescription() {
-        return description;
-    }
+  public static final String LAST_TOKEN_ID = "`last_token_id`";
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public static final String BANNER_URL = "`banner_url`";
 
-    public Long getLastTokenId() {
-        return lastTokenId;
-    }
+  public static final String GET_INFO_TIMES = "`get_info_times`";
 
-    public void setLastTokenId(Long lastTokenId) {
-        this.lastTokenId = lastTokenId;
-    }
+  public static final String IS_ROYALTIES = "`is_royalties`";
 
-    public String getBannerUrl() {
-        return bannerUrl;
-    }
+  public static final String SIGNER = "`signer`";
 
-    public void setBannerUrl(String bannerUrl) {
-        this.bannerUrl = bannerUrl;
-    }
+  public static final String IS_SYNC = "`is_sync`";
 
-    public Integer getGetInfoTimes() {
-        return getInfoTimes;
-    }
-
-    public void setGetInfoTimes(Integer getInfoTimes) {
-        this.getInfoTimes = getInfoTimes;
-    }
-
-    public Boolean getIsRoyalties() {
-        return isRoyalties;
-    }
-
-    public void setIsRoyalties(Boolean isRoyalties) {
-        this.isRoyalties = isRoyalties;
-    }
-
-    public String getSigner() {
-        return signer;
-    }
-
-    public void setSigner(String signer) {
-        this.signer = signer;
-    }
-
-    public Boolean getIsSync() {
-        return isSync;
-    }
-
-    public void setIsSync(Boolean isSync) {
-        this.isSync = isSync;
-    }
-
-    public static final String NAME = "`name`";
-
-    public static final String SYMBOL = "`symbol`";
-
-    public static final String ADDRESS = "`address`";
-
-    public static final String SHORT_URL = "`short_url`";
-
-    public static final String VERSION = "`version`";
-
-    public static final String COVER = "`cover`";
-
-    public static final String STORAGE_ID = "`storage_id`";
-
-    public static final String OWNER = "`owner`";
-
-    public static final String IS_ADMIN = "`is_admin`";
-
-    public static final String VERIFY = "`verify`";
-
-    public static final String DESCRIPTION = "`description`";
-
-    public static final String LAST_TOKEN_ID = "`last_token_id`";
-
-    public static final String BANNER_URL = "`banner_url`";
-
-    public static final String GET_INFO_TIMES = "`get_info_times`";
-
-    public static final String IS_ROYALTIES = "`is_royalties`";
-
-    public static final String SIGNER = "`signer`";
-
-    public static final String IS_SYNC = "`is_sync`";
-
-    @Override
-    public String toString() {
-        return "FcContract{" +
-        "name=" + name +
-        ", symbol=" + symbol +
-        ", address=" + address +
-        ", shortUrl=" + shortUrl +
-        ", version=" + version +
-        ", cover=" + cover +
-        ", storageId=" + storageId +
-        ", owner=" + owner +
-        ", isAdmin=" + isAdmin +
-        ", verify=" + verify +
-        ", description=" + description +
-        ", lastTokenId=" + lastTokenId +
-        ", bannerUrl=" + bannerUrl +
-        ", getInfoTimes=" + getInfoTimes +
-        ", isRoyalties=" + isRoyalties +
-        ", signer=" + signer +
-        ", isSync=" + isSync +
-        "}";
-    }
 }
